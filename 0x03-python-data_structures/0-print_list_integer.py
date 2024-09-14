0cb9eab39c3fd50b6003cbee6fe5fbb68a3b8b13
@Iam_Nyange <jnmajang@gmail.com>
2024-09-14 02:24:09 -0700

    update

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-""" 
-Prints each integer from the list 'my_list' on a new line.
-    Uses str.format() to format and print the integers.
-    
-Parameters:
-my_list (list): A list of integers to be printed.
-"""
+
 
 def print_list_integer(my_list=[]):
 
