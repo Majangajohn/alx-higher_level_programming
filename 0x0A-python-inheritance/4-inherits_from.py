696a689f6a1e4ee8ebc6da7734d8efad0f513685
@Iam_Nyange <jnmajang@gmail.com>
2024-10-14 04:55:07 -0700

    update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,6 +5,6 @@
 
 def inherits_from(obj, a_class):
     """ Check for direct or indirect inheritance """
-    if (type(obj) != a_class):
+    if (type(obj) is not a_class):
         return isinstance(obj, a_class)
     return False
